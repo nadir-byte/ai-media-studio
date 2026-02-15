7574e4a0770de73e21918c000caea44daf32f4b8
NM Thabatah <nmthabatah@NMs-Virtual-Machine.local>
2026-02-15 13:58:57 -0800

    Fix middleware: add public routes and fix matcher

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+export default clerkMiddleware({
+  // Public routes that don't require authentication
+  publicRoutes: ['/', '/sign-in', '/sign-up', '/api/webhook'],
+})
 
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)'],
+  matcher: ['/((?!_next|.*\\..*).*)'],
 }
