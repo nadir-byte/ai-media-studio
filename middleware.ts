fc58da79500e3a51656313de1a86ae0024338c5e
NM Thabatah <nmthabatah@NMs-Virtual-Machine.local>
2026-02-15 14:53:58 -0800

    Fix: Remove all route protection - use bare clerkMiddleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-// Define protected routes
-const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)',
-])
+export default clerkMiddleware()
 
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect()
-  }
-})
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
