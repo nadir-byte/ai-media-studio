8f5d6cb40901128c9e1b59063146db52c942f36c
NM Thabatah <nmthabatah@NMs-Virtual-Machine.local>
2026-02-15 13:30:57 -0800

    Simplify middleware to basic Clerk setup

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher([
-  '/',
-  '/sign-in(.*)',
-  '/sign-up(.*)',
-  '/api/webhooks(.*)',
-  '/api/health',
-  '/pricing',
-  '/pricing/self-hosted',
-])
-
-export default clerkMiddleware((auth, req) => {
-  // Public routes don't need auth
-  if (isPublicRoute(req)) {
-    return
-  }
-
-  // Protected routes - Clerk will handle auth
-  auth().protect()
-})
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 }
